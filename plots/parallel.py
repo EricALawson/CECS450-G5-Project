4b9751494ccc27b8fa4faa0dfe672f35d8696131
Brian Nguyen <brian.edison.nguyen@gmail.com>
2019-11-12 20:31:24 -0800

    Added scanpath data

--- plots/parallel.py
+++ plots/parallel.py
@@ -1,8 +1,11 @@
 import plotly.graph_objects as go
 import pandas as pd
 import preprocessing.additional_participant_data as meta_data
+import preprocessing.scanpath as scan_data
 
-aggData = meta_data.get_meta_data()
+
+df = meta_data.get_meta_data()
+aggData = scan_data.add_scanpath_length(df)
 
 fig = go.Figure(data=
     go.Parcoords(
@@ -14,10 +17,14 @@ fig = go.Figure(data=
                  constraintrange = [0.8, 1.2],
                 ticktext = ['TreeGeneral', 'GraphGeneral', 'TreeExpert', 'GraphExpert'],
                 label = 'Subset', values = aggData['Category']),
+
             dict(range = [0,1],
                 label = 'Task Success', values = aggData['Task_Success']),
+
             dict(range = [5, 75],
-                label = 'Total Time', values = aggData['Time_On_Task'])
+                label = 'Total Time', values = aggData['Time_On_Task']),
+            dict(range= [0, 20000],
+                 label= 'Scanpath', values=aggData['Scanpath_length'])
         ])
     )
 )
