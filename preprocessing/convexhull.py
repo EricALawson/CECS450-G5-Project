Fixed Convex Hull calculation. It now excludes the top ~35% or 400 pixels of the screen based on the assumption that those fixations are not related to the visualization. The axis range for the hull is based on the limited area 1600 * (1200 - 400) = 1,280,000

preprocessing/convexhull.py
@@ -18,6 +18,7 @@ import numpy as np
 import pandas as pd
 import os
 import preprocessing.additional_participant_data as meta_data
+import scipy.spatial as sp
 
 data_dir = '../eye_tracking_data'
 suffix = {1:'.treeFXD.txt', 2:'.graphFXD.txt'}
@@ -42,8 +43,11 @@ def add_convex_hull(aggData):
 
 
 def PolyArea(x,y):
-    return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
-
+    #return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
+    points = list(zip(x, y))
+    #print(points)
+    relevant_points = [ point for point in points if point[1] > 400] #point[1] is y, 400 is estimated end of a UI element on the screen
+    return sp.ConvexHull(relevant_points).volume
 
 
 
@@ -56,4 +60,5 @@ if __name__ == "__main__":
     df2 = add_convex_hull(df)
     print(df2)
     print(df2['Convex Hull Area'].max())
+
     